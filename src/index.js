709c03bb5e6067a939b4cfee71521583aea3cd69
uahuaman_pc2 <ul.huaman@gmail.com>
2024-03-12 14:25:23 +0100

    Main

--- src/index.js
+++ src/index.js
@@ -4,11 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Authentication from './pages/Authentication/Authentication';
+import MachineLearning from './pages/MachineLearning/MachineLearning';
+import Hosting from './pages/Hosting/Hosting';
+import Functions from './pages/Functions/Functions';
+import Database from './pages/Database/Database';
+import Storage from './pages/Storage/Storage';
+import { ThemeProvider } from '@mui/material/styles';
+import { dashboardTheme } from './dashboardTheme';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<ThemeProvider theme={dashboardTheme}>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="authentication" element={<Authentication />} />
+        <Route path="database" element={<Database />} />
+        <Route path="functions" element={<Functions />} />
+        <Route path="hosting" element={<Hosting />} />
+        <Route path="machine-learning" element={<MachineLearning />} />
+        <Route path="storage" element={<Storage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
